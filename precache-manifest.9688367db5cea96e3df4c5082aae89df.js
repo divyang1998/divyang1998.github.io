cb70e550b6f182e6f01229ea72a11b36198bcb3b
divyang1998 <divyang1998@gmail.com>
2020-03-04 23:51:34 -0500

    Updates

---
+++ precache-manifest.9688367db5cea96e3df4c5082aae89df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b282e65c95d4eeae680373e9ad0c5ff6",
+    "url": "/divyang1998.github.io/index.html"
+  },
+  {
+    "revision": "0fddd2cf6f04f4a04f1a",
+    "url": "/divyang1998.github.io/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "2a039db51bd28dc3375c",
+    "url": "/divyang1998.github.io/static/js/2.5b6f4b67.chunk.js"
+  },
+  {
+    "revision": "35e9035c4541049d41232fe0bf96f70b",
+    "url": "/divyang1998.github.io/static/js/2.5b6f4b67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fddd2cf6f04f4a04f1a",
+    "url": "/divyang1998.github.io/static/js/main.e849bbee.chunk.js"
+  },
+  {
+    "revision": "1370b600a62c96f013de",
+    "url": "/divyang1998.github.io/static/js/runtime-main.056a14e6.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d5faca4d19eb6e3e1f6666281a34e084.js"
+  "/divyang1998.github.io/precache-manifest.9688367db5cea96e3df4c5082aae89df.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/divyang1998.github.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
