259eb4b5ff2ece59b9b82e4b506fb37e66e2c263
divyang1998 <divyang1998@gmail.com>
2020-03-04 23:15:47 -0500

    Added the build script + homepage

---
+++ build/precache-manifest.d5faca4d19eb6e3e1f6666281a34e084.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a45485f0854efe484c587f220303db98",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4fd74f1165f346eccdef",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "2a039db51bd28dc3375c",
+    "url": "/static/js/2.5b6f4b67.chunk.js"
+  },
+  {
+    "revision": "35e9035c4541049d41232fe0bf96f70b",
+    "url": "/static/js/2.5b6f4b67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fd74f1165f346eccdef",
+    "url": "/static/js/main.e0dc7754.chunk.js"
+  },
+  {
+    "revision": "c2f98a5c387da539a513",
+    "url": "/static/js/runtime-main.5e682c5b.js"
+  }
+]);
